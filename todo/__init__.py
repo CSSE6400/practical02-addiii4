623f9d34edbbc866fef1706d7465398436992b33
addiii4 <adityacheema4@gmail.com>
2023-03-08 04:12:38 +1000

    added test cases

--- todo/__init__.py
+++ todo/__init__.py
@@ -3,15 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+    if config_overrides:
+        app.config.update(config_overrides)
     # Load the models
     from todo.models import db
     from todo.models.todo import Todo
     db.init_app(app)
     # Create the database tables
+    11
     with app.app_context():
         db.create_all()
         db.session.commit()
     # Register the blueprints
-    from todo.views.routes import api
+    from .views.routes import api
     app.register_blueprint(api)
+    
     return app
